dc5f8340e4e27334852aaac6b194dae0d52daa41
bman150 <63990801+bman150@users.noreply.github.com>
2021-08-01 23:39:35 -0700

    cipher 02

--- TextCipher.java
+++ TextCipher.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
-
+import java.io.*;
 /*A Java Program that lets user input text and prompt user to select
 a cipher to encrypt the text. Then the encrypted text is outputed.
 */
-import java.util.Scanner;
 public class TextCipher {
+    public static char normalChar[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+        'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    //For use in monoalphabetic cipher encryption
+    public static char codedChar[] = {'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O',
+    'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K',
+    'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'};
+
     //Ceasar cipher encryption
     public static StringBuffer caesarCipher(String text) {
         int s = 4;
@@ -23,12 +30,30 @@ public class TextCipher {
         return result;
     }
 
+    //Monoalphabetic cipher encryption
+    public static String monoAlphaCipher(String text) {
+        String encryptedString = "";
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = 0; j < 26; j++) {
+                if (text.charAt(i) == normalChar[j]) {
+                    encryptedString += codedChar[j];
+                    break;
+                }
+                if (text.charAt(i) < 'a' || text.charAt(i) > 'z') {
+                    encryptedString += text.charAt(i);
+                    break;
+                }
+            }
+        }
+        return encryptedString;
+    }
+
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
         System.out.print("Enter some text you want encrypted: "); //Prompt user for text.
         String userText = myScanner.nextLine();
         
-        System.out.println("Caesar");
+        System.out.println("Caesar, or Monoalphabetic");
         System.out.print("enter a cipher from the list above: "); //Prompt user to enter a chipher
         String userCipher = myScanner.nextLine();
 
@@ -36,6 +61,11 @@ public class TextCipher {
             System.out.println("Original Text: " + userText);
             System.out.println("Cipher: " + userCipher);
             System.out.println("Encryption: " + caesarCipher(userText));
+        }
+        else if(userCipher.equalsIgnoreCase("monoalphabetic")) {
+            System.out.println("Original Text: " + userText);
+            System.out.println("Cipher: " + userCipher);
+            System.out.println("Encryption: " + monoAlphaCipher(userText));
         }
         else {
             System.out.println("That is not an optional cipher");
